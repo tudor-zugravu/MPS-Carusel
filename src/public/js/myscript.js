2970cd91f121c872553ee5842a03c3d385c4b7ef
Alexandru Florea <alex.florea0106@gmail.com>
2015-11-14 17:41:56 +0200

    added random letters... again

--- src/public/js/myscript.js
+++ src/public/js/myscript.js
@@ -10,15 +10,32 @@ var dice9 = "IIUELP";
 
 $( document ).ready(function() {
     setWordInputBehaviour();
+    
     var randNum = Math.floor(Math.random() * 6);
     $('#letter1').html(dice1.charAt(randNum));
+    
+    randNum = Math.floor(Math.random() * 6);
     $('#letter2').html(dice2.charAt(randNum));
+    
+    randNum = Math.floor(Math.random() * 6);
     $('#letter3').html(dice3.charAt(randNum));
+    
+    randNum = Math.floor(Math.random() * 6);
     $('#letter4').html(dice4.charAt(randNum));
+    
+    randNum = Math.floor(Math.random() * 6);
     $('#letter5').html(dice5.charAt(randNum));
+    
+    randNum = Math.floor(Math.random() * 6);
     $('#letter6').html(dice6.charAt(randNum));
+    
+    randNum = Math.floor(Math.random() * 6);
     $('#letter7').html(dice7.charAt(randNum));
+    
+    randNum = Math.floor(Math.random() * 6);
     $('#letter8').html(dice8.charAt(randNum));
+    
+    randNum = Math.floor(Math.random() * 6);
     $('#letter9').html(dice9.charAt(randNum));
 });
 
