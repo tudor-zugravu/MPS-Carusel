57e5343105d987d61dbddc7d7cdb763375df1246
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-11-14 13:51:31 +0200

    callback on word input

--- src/public/js/myscript.js
+++ src/public/js/myscript.js
@@ -1,19 +1,28 @@
 
-var myDataRef = new Firebase('https://matei.firebaseio.com/');
-$('#messageInput').keypress(function (e) {
+// var myDataRef = new Firebase('https://matei.firebaseio.com/');
+// $('#messageInput').keypress(function (e) {
+//   if (e.keyCode == 13) {
+//     var name = $('#nameInput').val();
+//     var text = $('#messageInput').val();
+//     myDataRef.push({name: name, text: text});
+//     $('#messageInput').val('');
+//   }
+// });
+// myDataRef.on('child_added', function(snapshot) {
+//   console.log("child_added")
+//   var message = snapshot.val();
+//   displayChatMessage(message.name, message.text);
+// });
+// function displayChatMessage(name, text) {
+//   $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+//   $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+// };
+
+$('#wordInput').keypress(function (e) {
   if (e.keyCode == 13) {
     var name = $('#nameInput').val();
-    var text = $('#messageInput').val();
-    myDataRef.push({name: name, text: text});
-    $('#messageInput').val('');
+    var word = $('#wordInput').val();
+    $('#wordInput').val('');
+    console.log(name + " " + word)
   }
 });
-myDataRef.on('child_added', function(snapshot) {
-  console.log("child_added")
-  var message = snapshot.val();
-  displayChatMessage(message.name, message.text);
-});
-function displayChatMessage(name, text) {
-  $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
-  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-};
