1dbe651c6994eecc75b7cf2e43b9691242327122
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-11-14 16:43:34 +0200

    change color of input field

--- src/public/js/myscript.js
+++ src/public/js/myscript.js
@@ -1,32 +1,21 @@
-
-// var myDataRef = new Firebase('https://matei.firebaseio.com/');
-// $('#messageInput').keypress(function (e) {
-//   if (e.keyCode == 13) {
-//     var name = $('#nameInput').val();
-//     var text = $('#messageInput').val();
-//     myDataRef.push({name: name, text: text});
-//     $('#messageInput').val('');
-//   }
-// });
-// myDataRef.on('child_added', function(snapshot) {
-//   console.log("child_added")
-//   var message = snapshot.val();
-//   displayChatMessage(message.name, message.text);
-// });
-// function displayChatMessage(name, text) {
-//   $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
-//   $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-// };
-
-$('#letter1').html('litera1');
-
-$('#wordInput').keypress(function (e) {
+$('#wordInput').keypress(function(e) {
   if (e.keyCode == 13) {
     var word = $('#wordInput').val();
-    $('#wordInput').val('');
 
     $.get("/" + word, function(data) {
-      console.log("Data: " + data);
+      if (data == 'valid') {
+        $('#wordInput').css({'background-color': 'green'});
+        setTimeout(function() {
+          $('#wordInput').css({'background-color': 'white'});
+        }, 500)
+      } else {
+        $('#wordInput').css({'background-color': 'red'});
+        setTimeout(function() {
+          $('#wordInput').css({'background-color': 'white'});
+        }, 500)
+      }
+
+      $('#wordInput').val('');
     });
   }
 });
--- src/server.js
+++ src/server.js
@@ -3,17 +3,17 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 });
 
-app.get('/:word', function (req, res) {
+app.get('/:word', function(req, res) {
   console.log(req.params.word);
   // process
   res.send('valid');
 });
 
-var server = app.listen(80, function () {
+var server = app.listen(80, function() {
   var host = server.address().address;
   var port = server.address().port;
 
