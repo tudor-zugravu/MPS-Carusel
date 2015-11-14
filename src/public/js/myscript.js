305643808b2332368dc97bb6ebb52af44b3cc7e1
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-11-14 11:31:42 +0200

    first chat via firebase

---
+++ src/public/js/myscript.js
@@ -0,0 +1,19 @@
+
+var myDataRef = new Firebase('https://matei.firebaseio.com/');
+$('#messageInput').keypress(function (e) {
+  if (e.keyCode == 13) {
+    var name = $('#nameInput').val();
+    var text = $('#messageInput').val();
+    myDataRef.push({name: name, text: text});
+    $('#messageInput').val('');
+  }
+});
+myDataRef.on('child_added', function(snapshot) {
+  console.log("child_added")
+  var message = snapshot.val();
+  displayChatMessage(message.name, message.text);
+});
+function displayChatMessage(name, text) {
+  $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+};
