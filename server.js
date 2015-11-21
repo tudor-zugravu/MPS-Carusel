fde563d66a8806d6bf19415de3837372c13ac456
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-11-21 18:42:03 +0200

    ready

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ var maxHighscores = 10; /* Maximum number of highscores */
 var app = express();
 app.use(express.static('public'));
 
+app.set('port', (process.env.PORT || 5000));
+
 /* Main page */
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/" + "index.html" );
@@ -74,7 +76,7 @@ app.get('/:word', function(req, res) {
   }
 });
 
-var server = app.listen(80, function() {
+var server = app.listen(app.get('port'), function() {
   var host = server.address().address;
   var port = server.address().port;
 
